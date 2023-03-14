f375025f4fa9df46f1af89b82bd15f3487cc83f5
yanamt <53215000+yanamt@users.noreply.github.com>
2023-03-14 19:07:00 +0900

    blueprint作成

--- apps/crud/views.py
+++ apps/crud/views.py
@@ -1,3 +1,14 @@
 from flask import Blueprint,render_template
 
 #Blueprintオブジェクト（crudアプリ）の作成
+crud=Blueprint(
+    "crud",
+    __name__,
+    template_folder="templates",
+    static_folder="static",
+)
+
+#indexエンドポイントを作成しindex.htmlを返す
+@crud.route("/")
+def index():
+    return render_template("crud/index.html")
