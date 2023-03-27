0af9c6afee91fd06c4d283aa9d8d85f232e049b2
yanamt <53215000+yanamt@users.noreply.github.com>
2023-03-27 17:32:11 +0900

    データベース作成

--- apps/crud/models.py
+++ apps/crud/models.py
@@ -12,10 +12,17 @@ class User(db.Model):
     username=db.Column(db.String,index=True)
     email=db.Column(db.String,unique=True,index=True)
     password_hash=db.Column(db.String)
-    created_at=db.Column(db.Datetime,default=datetime.now)
-    updated_at=db.Column(db.Datetime,default=datetime.now,onupdate=datetime.now)
+    created_at=db.Column(db.DateTime,default=datetime.now)
+    updated_at=db.Column(db.DateTime,default=datetime.now,onupdate=datetime.now)
 
-    #パスワードをリセットするためのプロパティ
+    #パスワードをセットするためのプロパティ
+    #getter
     @property
     def password(self):
-        raise 
+        raise AttributeError("読み取り不可")
+    
+    @password.setter
+    def password(self,password):
+        self.password_hash=generate_password_hash(password)
+    
+    
